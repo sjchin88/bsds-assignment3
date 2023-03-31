07db14ee9b40bfd08fe7abc716f6c3377c9269ff
sjchin88 <jinqwem@hotmail.com>
2023-03-30 21:10:21 -0700

    update

--- Consumers/src/main/java/Recorder.java
+++ Consumers/src/main/java/Recorder.java
@@ -21,7 +21,7 @@ public class Recorder {
    */
   protected static final int NUM_THREAD = 20;
   protected static final int NUM_REDDIS_THREAD = 3;
-  protected static final int PREFETCH_COUNT = 1;
+  protected static final int PREFETCH_COUNT = 20;
   protected static final int BUFFER_SIZE = 500_000;
   /**
    * Default server address
